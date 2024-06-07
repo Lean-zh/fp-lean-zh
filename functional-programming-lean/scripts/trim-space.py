e687bb2670f30ff2453ede1c6b615c6aa48deb4a
Oling Cat <olingcat@gmail.com>
2024-06-07 16:29:06 +0800

    Trim extra spaces in generated html

---
+++ functional-programming-lean/scripts/trim-space.py
@@ -0,0 +1,19 @@
+import json
+import sys
+import re
+
+newline_re = re.compile(r'(?<=[，。、：）\)])\n(?=[\u4E00-\u9FFF`])')
+
+def trim_space(book):
+    sections = book['sections']
+    for ch in sections:
+        ch["Chapter"]["content"] = newline_re.sub("", ch['Chapter']['content'])
+    return book
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "supports":
+            sys.exit(0)
+
+    context, book = json.load(sys.stdin)
+    print(json.dumps(trim_space(book)))
