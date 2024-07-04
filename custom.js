2e6ae7a19bde8f1ff97e0911f5e0655643c5e4ed
OlingCat <olingcat@gmail.com>
2024-07-04 18:54:51 +0000

    Deploying to gh-pages from @ Lean-zh/fp-lean-zh@6093aa6f14dcc8a5d8d006b0236eb9298056836e 🚀

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 const newline = /(?<=[，。、：；」）》])\n(?!\n)/gu;
-const space = /\s*(<.+?>\p{Script=Han}.+?<\/.+?>)\s*/gu;
+const space = /\s+(<.+?>\p{Script=Han}.+?<\/.+?>)\s*/gu;
 const paras = document.querySelectorAll("#content > main > p");
 for (const p of paras) {
-  p.innerHTML = p.innerHTML.replace(newline, "");
-  p.innerHTML = p.innerHTML.replace(space, "$1");
+  p.innerHTML = p.innerHTML.replaceAll(newline, "");
+  p.innerHTML = p.innerHTML.replaceAll(space, "$1");
 }
