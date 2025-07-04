c1b6828f25221e85a72c858a1e8e6fb0cbfc3ce7
subfishzhou <subfishzhou@gmail.com>
2025-07-04 19:47:09 +0200

    fix gitignore

--- functional-programming-lean/custom.js
+++
@@ -1,7 +0,0 @@
-const newline = /(?<=[，。、：；」）》])\n(?!\n)/gu;
-const space = /\s+(<.+?>\p{Script=Han}.+?<\/.+?>)\s*/gu;
-const paras = document.querySelectorAll("#content > main > p");
-for (const p of paras) {
-  p.innerHTML = p.innerHTML.replaceAll(newline, "");
-  p.innerHTML = p.innerHTML.replaceAll(space, "$1");
-}
